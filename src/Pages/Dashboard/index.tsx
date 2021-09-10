8aa573e7ff685e569e54940272b212935d54e3a4
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-09-10 09:14:43 -0300

    Configuração do map para consumir a api

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Container } from './styles';
+import api from "../../service/api";
+
+import { Container, Box } from './styles';
+
+interface Pessoa {
+    nomePessoa: string;
+    telefonePessoa: string;
+}
 
 const Dashboard: React.FC = () => {
+    const [ pessoas, setPessoas ] = useState<Pessoa[]>([]);
+
+    useEffect(() => {
+        api.get(`/pessoas`).then(response => {
+            setPessoas(response.data);
+        });
+    }, []);
+
+    console.log(pessoas);
+
     return (
         <>
-            <Container>
-                <h1>asd</h1>
-            </Container>
+        <Container>
+            <h1>Dashboard</h1>
+
+            <Box>
+                {pessoas.map(pessoa => (
+                    <div>
+                        <p>{pessoa.nomePessoa}</p>
+                        <p>{pessoa.telefonePessoa}</p>
+                    </div>
+                ))}
+            </Box>
+        </Container>
         </>
     );
 }
